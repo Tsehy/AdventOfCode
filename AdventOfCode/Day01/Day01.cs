5bae0abea8d70d141c26d499a3d79270db71d189
Tsehy <cs.mate2000@gmail.com>
2022-12-01 23:08:05 +0100

    Day01 minor refactoring

--- AdventOfCode/Day01/Day01.cs
+++ AdventOfCode/Day01/Day01.cs
@@ -3,30 +3,13 @@
     private List<Elf> elves { get; set; }
 
     public Day01() : base("Day01")
-	{
-		elves = new List<Elf>();
+    {
+        elves = new List<Elf>();
 
-		int index = 0;
-		Elf elf = new Elf();
-
-		while (index < input.Length)
-		{
-            if (input[index] != "")
-            {
-				int calorie = int.Parse(input[index]);
-				elf.Add(calorie);
-            }
-            else
-            {
-                elves.Add(elf);
-                elf = new Elf();
-            }
-
-            index++;
-		}
+        ExtractData();
 
         elves = elves.OrderByDescending(e => e.TotalCalorie).ToList();
-	}
+    }
 
     public override void Part1()
 	{
@@ -46,4 +29,28 @@
 
         Console.WriteLine($"Maximum amount of calories carried by the top 3: {totalCalories}");
     }
+
+    #region Private methods
+    private void ExtractData()
+    {
+        int index = 0;
+        Elf elf = new Elf();
+
+        while (index < input.Length)
+        {
+            if (input[index] != "")
+            {
+                int calorie = int.Parse(input[index]);
+                elf.Add(calorie);
+            }
+            else
+            {
+                elves.Add(elf);
+                elf = new Elf();
+            }
+
+            index++;
+        }
+    }
+    #endregion
 }
