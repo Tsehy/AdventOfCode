ccf9d68b672324c5ed6e0c34f54902e7bd0d4cc5
Tsehy <cs.mate2000@gmail.com>
2022-12-02 19:11:44 +0100

    Day02 data restructure, using LINQ insted for loops

--- AdventOfCode/2022/Day02/2022Day02.cs
+++ AdventOfCode/2022/Day02/2022Day02.cs
@@ -7,8 +7,7 @@ using System.Threading.Tasks.Sources;
 
 public class _2022Day02 : _2022Day
 {
-    private List<string> opponent = new List<string>();
-    private List<string> guide = new List<string>();
+    private List<Tuple<char, char>> rounds = new(); // 1st opponent, 2nd guide
 
     public _2022Day02() : base("Day02")
     {
@@ -19,14 +18,18 @@ public class _2022Day02 : _2022Day
     {
         base.Part1();
 
-        Console.WriteLine($"The score you would get following the incorrect guide: {CalculateGames1(opponent, guide)}\n");
+        int totalScore = rounds.Select(r => CalculateRound1(r)).Sum();
+
+        Console.WriteLine($"The score you would get following the incorrect guide: {totalScore}\n");
     }
 
     public override void Part2()
     {
         base.Part2();
 
-        Console.WriteLine($"The score you would get following the correct guide: {CalculateGames2(opponent, guide)}\n");
+        int totalScore = rounds.Select(r => CalculateRound2(r)).Sum();
+
+        Console.WriteLine($"The score you would get following the correct guide: {totalScore}\n");
     }
 
     #region Private methods
@@ -35,76 +38,38 @@ public class _2022Day02 : _2022Day
         foreach (string data in input)
         {
             string[] tmp = data.Split(' ');
-            opponent.Add(tmp[0]);
-            guide.Add(tmp[1]);
+            rounds.Add(new Tuple<char, char>(tmp[0][0], tmp[1][0]));
         }
     }
 
-    private int CalculateGames1(List<string> opponent, List<string> guide)
+    private int CalculateRound1(Tuple<char, char> round)
     {
-        /* A, X - Rock
-         * B, Y - Paper
-         * C, Z - Scissors
-         */
-        int totalScore = 0;
+        int score = "-XYZ".IndexOf(round.Item1);
 
-        for (int i = 0; i < opponent.Count; i++)
-        {
-            totalScore += CalculateRound1(opponent[i], guide[i]);
-        }
+        string battle = $"{round.Item1}{round.Item2}";
 
-        return totalScore;
-    }
-
-    private int CalculateRound1(string opponent, string guide)
-    {
-        int score = "-XYZ".IndexOf(guide);
-
-        string battle = $"{opponent}{guide}";
-
-        // win
         if ("AYBZCX".Contains(battle))
-        {
+        {   // win
             score += 6;
         }
 
-        // tie
         if ("AXBYCZ".Contains(battle))
-        {
+        {   // tie
             score += 3;
         }
 
         return score;
     }
 
-    private int CalculateGames2(List<string> opponent, List<string> guide)
-    {
-        /* A - Rock
-         * B - Paper
-         * C - Scissors
-         * X - Need to lose
-         * Y - Need to tie
-         * Z - Need to win
-         */
-        int totalScore = 0;
-
-        for (int i = 0; i < opponent.Count; i++)
-        {
-            totalScore += CalculateRound2(opponent[i], guide[i]);
-        }
-
-        return totalScore;
-    }
-
-    private int CalculateRound2(string opponent, string guide)
+    private int CalculateRound2(Tuple<char, char> round)
     {
         int score = 0;
-        int opponentId = "-ABC".IndexOf(opponent);
+        int opponentId = "-ABC".IndexOf(round.Item1);
 
-        switch (guide)
+        switch (round.Item2)
         {
             // lose
-            case "X":
+            case 'X':
                 if (opponentId == 1)
                 {
                     opponentId = 4;
@@ -113,12 +78,12 @@ public class _2022Day02 : _2022Day
                 break; 
             
             // tie
-            case "Y":
+            case 'Y':
                 score += opponentId + 3;
                 break;
 
             // win
-            case "Z":
+            case 'Z':
                 if (opponentId == 3)
                 {
                     opponentId = 0;
