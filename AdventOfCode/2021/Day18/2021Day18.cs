45e5b6ac4364276557b176ff957cb3a65d223286
Tsehy <cs.mate2000@gmail.com>
2023-04-15 11:44:49 +0200

    Day18 competed

---
+++ AdventOfCode/2021/Day18/2021Day18.cs
@@ -0,0 +1,112 @@
+﻿public class _2021Day18 : _2021Day
+{
+    public _2021Day18() : base("Day18")
+    {
+        
+    }
+
+    public override void Part1()
+    {
+        base.Part1();
+
+        Node sumAll = Convert(input[0]);
+
+        for (int i = 1; i < input.Length; i++)
+        {
+            sumAll = new Node(sumAll, Convert(input[i]), null);
+            sumAll.Simplify();
+        }
+
+        Console.WriteLine($"The magnitude of the final sum: {sumAll.Magnitude()}\n");
+    }
+
+    public override void Part2()
+    {
+        base.Part2();
+
+        int maxMagnitude = 0;
+
+        for (int i = 0; i < input.Length; i++)
+        {
+            for (int j = i + 1; j < input.Length; j++)
+            {
+                maxMagnitude = GetMaxMagnitude(input[i], input[j], maxMagnitude);
+                maxMagnitude = GetMaxMagnitude(input[j], input[i], maxMagnitude);
+            }
+        }
+
+        Console.WriteLine($"The largest magnitude of two numbers: {maxMagnitude}\n");
+    }
+
+    #region Private methods
+    private static Node Convert(string input)
+    {
+        string trimInput = input.Substring(1, input.Length - 2);
+        int middle = MiddleCommaIndex(trimInput);
+
+        string left = trimInput.Substring(0, middle);
+        string right = trimInput.Substring(middle + 1, trimInput.Length - middle - 1);
+
+        Node leftNode;
+        if (left.Contains('['))
+        {
+            leftNode = Convert(left);
+        }
+        else
+        {
+            leftNode = new Node(int.Parse(left), null);
+        }
+
+        Node rightNode;
+        if (right.Contains('['))
+        {
+            rightNode = Convert(right);
+        }
+        else
+        {
+            rightNode = new Node(int.Parse(right), null);
+        }
+
+        return new Node(leftNode, rightNode, null);
+    }
+
+    private static int MiddleCommaIndex(string input)
+    {
+        int depth = 0;
+
+        for (int i = 0; i < input.Length; i++)
+        {
+            switch (input[i])
+            {
+                case '[':
+                    depth++;
+                    break;
+                case ']':
+                    depth--;
+                    break;
+                case ',':
+                    if (depth == 0)
+                    {
+                        return i;
+                    }
+                    break;
+            }
+        }
+
+        return -1;
+    }
+
+    private static int GetMaxMagnitude(string a, string b, int currentMax)
+    {
+        Node sum = new Node(Convert(a), Convert(b), null);
+        sum.Simplify();
+        int m = sum.Magnitude();
+        if (m > currentMax)
+        {
+            return m;
+        }
+
+        return currentMax;
+    }
+    #endregion
+}
