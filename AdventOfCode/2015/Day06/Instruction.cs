b93480a7de0b1fd0f834bdeac4a85fd35434c716
Tsehy <cs.mate2000@gmail.com>
2024-12-16 18:44:11 +0100

    2015: Day06 completed;

---
+++ AdventOfCode/2015/Day06/Instruction.cs
@@ -0,0 +1,21 @@
+﻿using System.Text.RegularExpressions;
+
+namespace AdventOfCode.Day06
+{
+    readonly record struct Instruction(string Mode, int X1, int Y1, int X2, int Y2)
+    {
+        public static implicit operator Instruction(string stringInstruction)
+        {
+            var r = new Regex(@"(toggle|turn on|turn off) (\d{0,3}),(\d{0,3}) through (\d{0,3}),(\d{0,3})");
+            Match m = r.Match(stringInstruction);
+            return new Instruction(
+                m.Groups[1].Value,
+                int.Parse(m.Groups[2].Value),
+                int.Parse(m.Groups[3].Value),
+                int.Parse(m.Groups[4].Value),
+                int.Parse(m.Groups[5].Value)
+            );
+        }
+    }
+
+}
