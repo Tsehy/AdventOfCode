899c525eb51f22da1c78237f8985cc56d719716e
Tsehy <cs.mate2000@gmail.com>
2025-01-10 13:06:13 +0100

    2015 Day 17 completed;

---
+++ AdventOfCode/2015/Day17/2015Day17.cs
@@ -0,0 +1,50 @@
+﻿using AdventOfCode._2015.Day17;
+
+namespace AdventOfCode
+{
+    public class _2015Day17 : _2015Day
+    {
+        private readonly List<Container> Containers = [];
+        private readonly List<string> FoundSolutions = [];
+        private string Solutions => string.Join(' ', Containers.Select((c, i) => (c, i)).Where(g => g.c.Used).Select(g => g.i));
+
+        public _2015Day17() : base("Day17")
+        {
+            foreach (string line in Input.OrderBy(c => c))
+            {
+                Containers.Add(new (int.Parse(line)));
+            }
+            Reduce(150);
+        }
+
+        public override void Part1()
+        {
+            base.Part1();
+
+            Console.WriteLine($"Total combinations of storing the eggnog: {FoundSolutions.Count}\n");
+        }
+
+        public override void Part2()
+        {
+            base.Part2();
+
+            int min = FoundSolutions.Min(s => s.Count(c => c == ' '));
+            Console.WriteLine($"Total combinations of storing the eggnog using the least amount of containers: {FoundSolutions.Count(s => s.Count(c => c == ' ') == min)}\n");
+        }
+
+        private void Reduce(int value)
+        {
+            if (value == 0 && !FoundSolutions.Contains(Solutions))
+            { 
+                FoundSolutions.Add(Solutions);
+                return;
+            }
+            foreach (Container container in Containers.Where(c => !c.Used && c.Size <= value))
+            {
+                container.Used = true;
+                Reduce(value - container.Size);
+                container.Used = false;
+            }
+        }
+    }
+}
---
+++ AdventOfCode/2015/Day17/Container.cs
@@ -0,0 +1,8 @@
+﻿namespace AdventOfCode._2015.Day17
+{
+    public class Container(int size)
+    {
+        public int Size { get; init; } = size;
+        public bool Used { get; set; } = false;
+    }
+}
