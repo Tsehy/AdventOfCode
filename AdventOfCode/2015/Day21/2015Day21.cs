1c18d0a53575edba3510ffdf43b10a51d4818d0e
Tsehy <cs.mate2000@gmail.com>
2025-12-07 18:39:03 +0100

    2015 Day 21 completed;

---
+++ AdventOfCode/2015/Day21/2015Day21.cs
@@ -0,0 +1,58 @@
+﻿using AdventOfCode._2015.Day21;
+
+namespace AdventOfCode
+{
+    public class _2015Day21 : _2015Day
+    {
+        private readonly int LowestCost = int.MaxValue;
+        private readonly int HighestCost = int.MinValue;
+
+        public _2015Day21() : base("Day21")
+        {
+            int bossHealth = int.Parse(Input[0].Split(": ")[1]);
+            int bossDamage = int.Parse(Input[1].Split(": ")[1]);
+            int bossArmor = int.Parse(Input[2].Split(": ")[1]);
+
+            List<Equipment> Weapons = [new Equipment(8, 4, 0), new Equipment(10, 5, 0), new Equipment(25, 6, 0), new Equipment(40, 7, 0), new Equipment(74, 8, 0)];
+            List<Equipment?> Armors = [null, new Equipment(13, 0, 1), new Equipment(31, 0, 2), new Equipment(53, 0, 3), new Equipment(75, 0, 4), new Equipment(102, 0, 5)];
+            List<Equipment?> Rings = [null, null, new Equipment(25, 1, 0), new Equipment(50, 2, 0), new Equipment(100, 3, 0), new Equipment(20, 0, 1), new Equipment(40, 0, 2), new Equipment(80, 0, 3)];
+
+            var setup = new EquipmentSetup();
+            foreach (Equipment weapon in Weapons)
+            {
+                setup.Weapon = weapon;
+                foreach (Equipment? armor in Armors)
+                {
+                    setup.Armor = armor;
+                    for (int i = 0; i < Rings.Count - 1; i++)
+                    {
+                        setup.LeftRing = Rings[i];
+                        for (int j = i + 1; j < Rings.Count; j++)
+                        {
+                            setup.RightRing = Rings[j];
+                            if (setup.CanWinFight(bossHealth, bossDamage, bossArmor) && LowestCost > setup.TotalPrice)
+                                LowestCost = setup.TotalPrice;
+
+                            if (!setup.CanWinFight(bossHealth, bossDamage, bossArmor) && HighestCost < setup.TotalPrice)
+                                HighestCost = setup.TotalPrice;
+                        }
+                    }
+                }
+            }
+        }
+
+        public override void Part1()
+        {
+            base.Part1();
+
+            Console.WriteLine(LowestCost);
+        }
+
+        public override void Part2()
+        {
+            base.Part2();
+
+            Console.WriteLine(HighestCost);
+        }
+    }
+}
---
+++ AdventOfCode/2015/Day21/Equipment.cs
@@ -0,0 +1,38 @@
+﻿namespace AdventOfCode._2015.Day21
+{
+    public class Equipment(int price, int damage, int armor)
+    {
+        public int Price { get { return price; } }
+        public int Damage { get { return damage; } }
+        public int Armor { get { return armor; } }
+    }
+
+    public class EquipmentSetup
+    {
+        public Equipment? Weapon { get; set; }
+        public Equipment? Armor { get; set; }
+        public Equipment? LeftRing { get; set; }
+        public Equipment? RightRing { get; set; }
+
+        public int TotalDamage => (Weapon?.Damage ?? 0) + (Armor?.Damage ?? 0) + (LeftRing?.Damage ?? 0) + (RightRing?.Damage ?? 0);
+        public int TotalArmor => (Weapon?.Armor ?? 0) + (Armor?.Armor ?? 0) + (LeftRing?.Armor ?? 0) + (RightRing?.Armor ?? 0);
+        public int TotalPrice => (Weapon?.Price ?? 0) + (Armor?.Price ?? 0) + (LeftRing?.Price ?? 0) + (RightRing?.Price ?? 0);
+
+        public int TurnsToKill(int health, int armor)
+        {
+            int realDamage = Math.Max(1, TotalDamage - armor);
+            return (int)Math.Ceiling((double)health / realDamage);
+        }
+
+        public int TurnsToDie(int damage)
+        {
+            int realDamage = Math.Max(1, damage - TotalArmor);
+            return (int)Math.Ceiling(100.0 / realDamage);
+        }
+
+        public bool CanWinFight(int health, int damage, int armor)
+        {
+            return TurnsToKill(health, armor) <= TurnsToDie(damage);
+        }
+    }
+}
