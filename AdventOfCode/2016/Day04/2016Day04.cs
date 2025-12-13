cab837e86ac464eebb801d2760fca51e725338b3
Tsehy <cs.mate2000@gmail.com>
2025-12-13 11:11:18 +0100

    2016 Day 04 completed;

---
+++ AdventOfCode/2016/Day04/2016Day04.cs
@@ -0,0 +1,31 @@
+﻿using AdventOfCode._2016.Day04;
+
+namespace AdventOfCode;
+
+public class _2016Day04 : _2016Day
+{
+    private readonly List<Room> Rooms;
+
+    public _2016Day04() : base("Day04")
+    {
+        Rooms = [.. Input.Select(i => new Room(i))];
+    }
+
+    public override void Part1()
+    {
+        base.Part1();
+
+        int realRooms = Rooms.Where(r => r.IsValid()).Sum(r => r.SectorId);
+
+        Console.WriteLine($"Sum of the real rooms sector ids: {realRooms}\n");
+    }
+
+    public override void Part2()
+    {
+        base.Part2();
+
+        int sectorId = Rooms.FirstOrDefault(r => r.IsValid() && r.Decrypt() == "northpole-object-storage")?.SectorId ?? 0;
+
+        Console.WriteLine($"Sector id of the north pole objects: {sectorId}");
+    }
+}
---
+++ AdventOfCode/2016/Day04/Room.cs
@@ -0,0 +1,48 @@
+﻿using System.Text;
+using System.Text.RegularExpressions;
+
+namespace AdventOfCode._2016.Day04;
+
+public partial class Room
+{
+    public string EncryptedParts { get; set; }
+    public int SectorId { get; set; }
+    public string CheckSum { get; set; }
+
+    public Room(string raw)
+    {
+        var parts = RoomRegex().Match(raw);
+        EncryptedParts = parts.Groups[1].Value;
+        SectorId = int.Parse(parts.Groups[2].Value);
+        CheckSum = parts.Groups[3].Value;
+    }
+
+    [GeneratedRegex(@"([a-z-]+)-(\d+)\[([a-z]{5})\]")]
+    private static partial Regex RoomRegex();
+
+    public bool IsValid()
+    {
+        return string.Join("", EncryptedParts.Where(p => p != '-').GroupBy(p => p).OrderByDescending(g => g.Count()).ThenBy(g => g.Key).Take(5).Select(g => g.Key)) == CheckSum;
+    }
+
+    public string Decrypt()
+    {
+        int rotate = SectorId % 26;
+        var sb = new StringBuilder();
+        foreach (char part in EncryptedParts)
+        {
+            if (part == '-')
+            {
+                sb.Append(part);
+            }
+            else
+            {
+                int decrypted = part + rotate;
+                if (decrypted > 'z')
+                    decrypted -= 26;
+                sb.Append((char)decrypted);
+            }
+        }
+        return sb.ToString();
+    }
+}
