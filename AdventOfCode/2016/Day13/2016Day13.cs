2e998a72513ddbe756165c931c452d5b477ba98f
Tsehy <cs.mate2000@gmail.com>
2025-12-22 14:57:50 +0100

    2016 Day 13 completed;

---
+++ AdventOfCode/2016/Day13/2016Day13.cs
@@ -0,0 +1,78 @@
+﻿namespace AdventOfCode._2016.Day13;
+
+public class _2016Day13 : _2016Day
+{
+    private readonly int Secret;
+
+    public _2016Day13() : base("Day13")
+    {
+        Secret = int.Parse(Input[0]);
+    }
+
+    private int MinMoves(Point start, Point goal)
+    {
+        var open = new PriorityQueue<Point, int>();
+        var best = new Dictionary<Point, int>();
+
+        open.Enqueue(start, 0);
+        best[start] = start.Moves;
+
+        while (open.Count > 0)
+        {
+            var current = open.Dequeue();
+
+            if (current == goal)
+                return current.Moves;
+
+            foreach (var neighbour in current.Neighbours(Secret))
+            {
+                if (!best.TryGetValue(neighbour, out int prevMoves) || neighbour.Moves < prevMoves)
+                {
+                    best[neighbour] = neighbour.Moves;
+                    open.Enqueue(neighbour, neighbour.Moves + neighbour.GetHeuristic(goal));
+                }
+            }
+        }
+
+        return -1;
+    }
+
+    public override void Part1()
+    {
+        base.Part1();
+
+        int min = MinMoves(new Point(1, 1), new Point(31, 39));
+        Console.WriteLine($"Fewest moves to the goal: {min}");
+    }
+
+    private int ReachablePoints(Point start, int moves)
+    {
+        var open = new Queue<Point>();
+        var visited = new HashSet<Point>();
+
+        open.Enqueue(start);
+
+        while (open.Count > 0)
+        {
+            var current = open.Dequeue();
+            visited.Add(current);
+
+            if (current.Moves >= moves)
+                continue;
+
+            foreach (var neighbour in current.Neighbours(Secret))
+                if (!visited.Contains(neighbour))
+                    open.Enqueue(neighbour);
+        }
+
+        return visited.Count;
+    }
+
+    public override void Part2()
+    {
+        base.Part2();
+
+        int count = ReachablePoints(new Point(1, 1), 50);
+        Console.WriteLine($"Number of reachable points: {count}");
+    }
+}
---
+++ AdventOfCode/2016/Day13/Point.cs
@@ -0,0 +1,65 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace AdventOfCode._2016.Day13;
+
+internal readonly struct Point(int x, int y, int moves = 0)
+{
+    public readonly int X { get; } = x;
+    public readonly int Y { get; } = y;
+    public readonly int Moves { get; } = moves;
+
+    public int GetHeuristic(Point goal) => Math.Abs(X - goal.X) + Math.Abs(Y + goal.Y);
+
+    public bool IsWall(int secret)
+    {
+        int num = X * X + 3 * X + 2 * X * Y + Y + Y * Y + secret;
+        return Convert.ToString(num, 2).Count(c => c == '1') % 2 == 1;
+    }
+
+    public IEnumerable<Point> Neighbours(int secret)
+    {
+        int[] directions = [-1, 1];
+
+        foreach (int xMove in directions)
+        {
+            int newX = X + xMove;
+            if (newX < 0)
+                continue;
+
+            var np = new Point(newX, Y, Moves + 1);
+            if (np.IsWall(secret))
+                continue;
+
+            yield return np;
+        }
+
+        foreach (int yMove in directions)
+        {
+            int newY = Y + yMove;
+            if (newY < 0)
+                continue;
+
+            var np = new Point(X, newY, Moves + 1);
+            if (np.IsWall(secret))
+                continue;
+
+            yield return np;
+        }
+    }
+
+    public override string ToString() => $"{X}:{Y} - {Moves}";
+    public override int GetHashCode() => HashCode.Combine(X, Y);
+    public override bool Equals([NotNullWhen(true)] object? obj)
+    {
+        if (obj == null)
+            return false;
+
+        if (obj is Point other)
+            return X == other.X && Y == other.Y;
+
+        return false;
+    }
+
+    public static bool operator ==(Point left, Point right) => left.Equals(right);
+    public static bool operator !=(Point left, Point right) => !left.Equals(right);
+}
