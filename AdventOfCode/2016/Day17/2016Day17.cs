8a83d4ddd5d0ab24af0497d044850fd42f0c88d4
Tsehy <cs.mate2000@gmail.com>
2025-12-22 19:32:36 +0100

    2016 Day 17 completed;

---
+++ AdventOfCode/2016/Day17/2016Day17.cs
@@ -0,0 +1,86 @@
+﻿namespace AdventOfCode._2016.Day17;
+
+public class _2016Day17 : _2016Day
+{
+    public readonly string Password;
+
+    public _2016Day17() : base("Day17")
+    {
+        Password = Input[0];
+    }
+
+    private static string ShortestPath(int startX, int startY, string password, int goalX, int goalY)
+    {
+        var start = new Position(startX, startY, password, "");
+
+        var open = new PriorityQueue<Position, int>();
+        var best = new Dictionary<Position, int>();
+
+        open.Enqueue(start, 0);
+        best[start] = start.Moves;
+
+        while (open.Count > 0)
+        {
+            var current = open.Dequeue();
+
+            if (current.Heuristic(goalX, goalY) == 0)
+                return current.Path;
+
+            foreach (var neighbour in current.Neighbours(password))
+            {
+                if (!best.TryGetValue(neighbour, out int prevMoves) || neighbour.Moves < prevMoves)
+                {
+                    best[neighbour] = neighbour.Moves;
+                    open.Enqueue(neighbour, neighbour.Moves + neighbour.Heuristic(goalX, goalY));
+                }
+            }
+        }
+
+        return string.Empty;
+    }
+
+    public override void Part1()
+    {
+        base.Part1();
+
+        string path = ShortestPath(0, 0, Password, 3, 3);
+        Console.WriteLine($"The shortest path is: {path}");
+    }
+
+    private static int LongestPath(int startX, int startY, string password, int goalX, int goalY)
+    {
+        var start = new Position(startX, startY, password, "");
+
+        var open = new PriorityQueue<Position, int>();
+        int max = int.MinValue;
+
+        open.Enqueue(start, 0);
+
+        while (open.Count > 0)
+        {
+            var current = open.Dequeue();
+
+            if (current.Heuristic(goalX, goalY) == 0)
+            {
+                if (current.Moves > max)
+                    max = current.Moves;
+                continue;
+            }
+
+            foreach (var neighbour in current.Neighbours(password))
+            {
+                open.Enqueue(neighbour, neighbour.Moves + neighbour.Heuristic(goalX, goalY));
+            }
+        }
+
+        return max;
+    }
+
+    public override void Part2()
+    {
+        base.Part2();
+
+        int max = LongestPath(0, 0, Password, 3, 3);
+        Console.WriteLine($"The length of the longest path is: {max}");
+    }
+}
---
+++ AdventOfCode/2016/Day17/Position.cs
@@ -0,0 +1,65 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace AdventOfCode._2016.Day17;
+
+internal class Position(int x, int y, string password, string path)
+{
+    public int X { get; set; } = x;
+    public int Y { get; set; } = y;
+    public string Path { get; set; } = path;
+    public string Hash { get; set; } = Convert.ToHexString(MD5.HashData(Encoding.UTF8.GetBytes(password + path)))[..4].ToLower();
+    public int Moves => Path.Length;
+
+    public int Heuristic(int x, int y) => Math.Abs(x - X) + Math.Abs(y - Y);
+
+    public IEnumerable<Position> Neighbours(string password)
+    {
+        var doors = Hash.Select((c, i) => (DoorIndex: i, IsOpen: c > 'a')).Where(p => p.IsOpen);
+        foreach ((int doorIndex, _) in doors)
+        {
+            int newX = X, newY = Y;
+            string newPath = Path;
+            switch (doorIndex)
+            {
+                case 0:
+                    newY--;
+                    newPath += 'U';
+                    break;
+
+                case 1:
+                    newY++;
+                    newPath += 'D';
+                    break;
+
+                case 2:
+                    newX--;
+                    newPath += 'L';
+                    break;
+
+                case 3:
+                    newX++;
+                    newPath += 'R';
+                    break;
+            }
+
+            if (newX < 0 || newX > 3 || newY < 0 || newY > 3)
+                continue;
+
+            yield return new Position(newX, newY, password, newPath);
+        }
+    }
+
+    public override int GetHashCode() => HashCode.Combine(X, Y, Hash);
+    public override bool Equals([NotNullWhen(true)]object? obj)
+    {
+        if (obj == null)
+            return false;
+
+        if (obj is Position other)
+            return other.Y == Y && other.X == X && other.Hash == Hash;
+
+        return false;
+    }
+}
