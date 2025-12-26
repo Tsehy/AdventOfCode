9af5c32ce568af64990ed357ed7ec447b86e02b2
Tsehy <cs.mate2000@gmail.com>
2025-12-26 11:49:21 +0100

    2017 Day 01 completed;

---
+++ AdventOfCode/2017/2017Day.cs
@@ -0,0 +1,8 @@
+﻿namespace AdventOfCode._2017;
+
+public class _2017Day : Day
+{
+    public _2017Day(string folderName) : base("2017", folderName)
+    {
+    }
+}
---
+++ AdventOfCode/2017/Day01/2017Day01.cs
@@ -0,0 +1,38 @@
+﻿namespace AdventOfCode._2017.Day01;
+
+public  class _2017Day01 : _2017Day
+{
+    public _2017Day01() : base("Day01")
+    {
+    }
+
+    private int CalculateChapta(int away = 1)
+    {
+        int sum = 0;
+        for (int i = 0; i < Input[0].Length; i++)
+        {
+            int next = (i + away) % Input[0].Length;
+            char curr = Input[0][i];
+            if (curr == Input[0][next])
+                sum += curr - '0';
+        }
+        return sum;
+    }
+
+    public override void Part1()
+    {
+        base.Part1();
+
+        int sum = CalculateChapta();
+        Console.WriteLine($"The chapta is: {sum}");
+    }
+
+    public override void Part2()
+    {
+        base.Part2();
+
+        int away = Input[0].Length / 2;
+        int sum = CalculateChapta(away);
+        Console.WriteLine($"The chapta is: {sum}");
+    }
+}
