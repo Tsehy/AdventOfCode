f6e6657852e3ba3fffcfe8fe4304a0d8d3a0d9d4
Tsehy <cs.mate2000@gmail.com>
2025-12-28 18:55:29 +0100

    2017 Day 10 completed;

---
+++ AdventOfCode/2017/Day10/2017Day10.cs
@@ -0,0 +1,65 @@
+﻿using System.Text;
+
+namespace AdventOfCode._2017.Day10;
+
+public class _2017Day10 : _2017Day
+{
+    private readonly int[] Appendix = [17, 31, 73, 47, 23];
+
+    public _2017Day10() : base("Day10")
+    {
+    }
+
+    private static void KnotHashStep(CircularList cList, List<int> bytes, ref int position, ref int skipSize)
+    {
+        foreach (int length in bytes)
+        {
+            cList.Twist(position, length);
+            position = (position + length + skipSize) % cList.Count;
+            skipSize++;
+        }
+    }
+
+    private string KnotHash(string plainText)
+    {
+        var cList = new CircularList(256);
+        int position = 0, skipSize = 0;
+        List<int> bytes = [.. Encoding.ASCII.GetBytes(plainText).Select(b => (int)b)];
+        bytes.AddRange(Appendix);
+
+        for (int i = 0; i < 64; i++)
+            KnotHashStep(cList, bytes, ref position, ref skipSize);
+
+        byte[] sparseHash = [.. cList.ToList().Select(i => (byte)i)];
+        byte[] denseHash = new byte[16];
+        for (int i = 0; i < 16; i++)
+        {
+            byte num = sparseHash[i * 16];
+            for (int j = 1; j < 16; j++)
+                num = (byte)(num ^ sparseHash[i * 16 + j]);
+            denseHash[i] = num;
+        }
+
+        return Convert.ToHexString(denseHash).ToLower();
+    }
+
+    public override void Part1()
+    {
+        base.Part1();
+
+        List<int> lengths = [.. Input[0].Split(',').Select(int.Parse)];
+        var cList = new CircularList(256);
+        int position = 0, skipSize = 0;
+        KnotHashStep(cList, lengths, ref position, ref skipSize);
+
+        Console.WriteLine($"The checksum is: {cList[0] * cList[1]}");
+    }
+
+    public override void Part2()
+    {
+        base.Part2();
+
+        string cipher = KnotHash(Input[0].Trim());
+        Console.WriteLine($"The Knot Hash is: {cipher}");
+    }
+}
---
+++ AdventOfCode/2017/Day10/CircularList.cs
@@ -0,0 +1,84 @@
+﻿namespace AdventOfCode._2017.Day10;
+
+internal class CircularNode(int value, CircularNode next, CircularNode prev)
+{
+    public int Value { get; set; } = value;
+    public CircularNode Next { get; set; } = next;
+    public CircularNode Previous { get; set; } = prev;
+}
+
+internal class CircularList
+{
+    private readonly Dictionary<int, CircularNode> Nodes = [];
+
+    public CircularNode First => Nodes[0];
+    public CircularNode Last => Nodes[Nodes.Count - 1];
+    public int Count => Nodes.Count;
+
+    public CircularList()
+    {
+    }
+
+    public CircularList(int count) : base()
+    {
+        if (count <= 0)
+            return;
+
+        AddFirst(0);
+
+        for (int i = 1; i < count; i++)
+            Add(i);
+    }
+
+    public int this[int index] { get => Nodes[index].Value; set => Nodes[index].Value = value; }
+
+    public void Add(int item)
+    {
+        int newIndex = Nodes.Count;
+        var first = First;
+        var last = Last;
+        var newNode = new CircularNode(item, first, last);
+        Nodes[newIndex] = newNode;
+        first.Previous = last.Next = newNode;
+    }
+
+    public void AddFirst(int value)
+    {
+        if (Nodes.Count > 0)
+            throw new InvalidOperationException("List is not empty!");
+
+        Nodes[0] = new CircularNode(value, null!, null!);
+        Nodes[0].Next = Nodes[0].Previous = Nodes[0];
+    }
+
+    public void Twist(int index, int length)
+    {
+        int startIndex = index % Nodes.Count;
+        int endIndex = (startIndex + length - 1) % Nodes.Count;
+
+        var startNode = Nodes[startIndex];
+        var endNode = Nodes[endIndex];
+
+        var beforeNode = startNode.Previous;
+        var afterNode = endNode.Next;
+
+        beforeNode.Next = endNode;
+        endNode.Next = beforeNode;
+
+        afterNode.Previous = startNode;
+        startNode.Previous = afterNode;
+
+        int reIndex = startIndex;
+        var current = endNode;
+        for (int i = 0; i < length; i++)
+        {
+            (current.Previous, current.Next) = (current.Next, current.Previous);
+            Nodes[reIndex] = current;
+
+            reIndex = (reIndex + 1) % Nodes.Count;
+            current = current.Next;
+        }
+    }
+
+    public List<int> ToList() => [.. Nodes.Values.Select(i => i.Value)];
+}
