abd1468f5607a46917b9116fe7f307bef97b9ea8
Tsehy <cs.mate2000@gmail.com>
2025-12-30 21:34:06 +0100

    2017 Day 18 completed;

---
+++ AdventOfCode/2017/Day18/2017Day18.cs
@@ -0,0 +1,53 @@
+﻿namespace AdventOfCode._2017.Day18;
+
+public class _2017Day18 : _2017Day
+{
+    private readonly List<Command> Commands = [];
+
+    public _2017Day18() : base("Day18")
+    {
+        foreach (string input in Input)
+        {
+            string[] parts = input.Split(' ');
+            var type = (CommandType)Enum.Parse(typeof(CommandType), parts[0]);
+            if (parts.Length == 3)
+                Commands.Add(new(type, parts[1], parts[2]));
+            else
+                Commands.Add(new(type, parts[1], "0"));
+        }
+    }
+
+    public override void Part1()
+    {
+        base.Part1();
+
+        var i = new Interpreter(Commands);
+        do
+            i.Step();
+        while (!i.IsBlocked);
+
+        Console.WriteLine($"The recovered sound is: {i.Outgoing.Last()}.");
+    }
+
+    public override void Part2()
+    {
+        base.Part2();
+
+        var i0 = new Interpreter(Commands, 0);
+        var i1 = new Interpreter(Commands, 1);
+
+        i0.Incoming = i1.Outgoing;
+        i1.Incoming = i0.Outgoing;
+
+        bool progress;
+        do
+        {
+            progress = false;
+            progress |= i0.Step();
+            progress |= i1.Step();
+        }
+        while (progress);
+
+        Console.WriteLine($"Program 1 sent {i1.SendCount} values.");
+    }
+}
---
+++ AdventOfCode/2017/Day18/Interpreter.cs
@@ -0,0 +1,123 @@
+﻿namespace AdventOfCode._2017.Day18;
+
+internal enum CommandType
+{
+    snd, set, add, mul, mod, rcv, jgz
+}
+
+internal struct Command
+{
+    public CommandType Type;
+    public int X;
+    public int Y;
+    public bool XIsRegister;
+    public bool YIsRegister;
+
+    public Command(CommandType type, string reg1, string reg2)
+    {
+        Type = type;
+        
+        if (int.TryParse(reg1, out int x))
+        {
+            X = x;
+            XIsRegister = false;
+        }
+        else
+        {
+            X = reg1[0] - 'a';
+            XIsRegister = true;
+        }
+
+        if (int.TryParse(reg2, out int y))
+        {
+            Y = y;
+            YIsRegister = false;
+        }
+        else
+        {
+            Y = reg2[0] - 'a';
+            YIsRegister = true;
+        }
+    }
+}
+
+internal class Interpreter
+{
+    private readonly List<Command> Commands;
+    private readonly long[] Reg = new long[26];
+
+    private int CommandPointer = 0;
+
+    public Queue<long> Incoming { get; set; } = [];
+    public Queue<long> Outgoing { get; private set; } = [];
+
+    public int SendCount { get; private set; } = 0;
+    public bool IsBlocked { get; private set; }
+
+    public Interpreter(List<Command> commands, int id = 0)
+    {
+        Commands = commands;
+        Reg['p' - 'a'] = id;
+    }
+
+    public bool Step()
+    {
+        if (0 > CommandPointer || CommandPointer >= Commands.Count)
+        {
+            IsBlocked = true;
+            return false;
+        }
+
+        var cmd = Commands[CommandPointer];
+        switch (cmd.Type)
+        {
+            case CommandType.snd:
+                Outgoing.Enqueue(Reg[cmd.X]);
+                SendCount++;
+                CommandPointer++;
+                break;
+
+            case CommandType.set:
+                Reg[cmd.X] = ResolveY(cmd);
+                CommandPointer++;
+                break;
+
+            case CommandType.add:
+                Reg[cmd.X] += ResolveY(cmd);
+                CommandPointer++;
+                break;
+
+            case CommandType.mul:
+                Reg[cmd.X] *= ResolveY(cmd);
+                CommandPointer++;
+                break;
+
+            case CommandType.mod:
+                Reg[cmd.X] %= ResolveY(cmd);
+                CommandPointer++;
+                break;
+
+            case CommandType.rcv:
+                if (Incoming.Count == 0)
+                {
+                    IsBlocked = true;
+                    return false;
+                }
+                Reg[cmd.X] = Incoming.Dequeue();
+                CommandPointer++;
+                break;
+
+            case CommandType.jgz:
+                if ((cmd.XIsRegister ? Reg[cmd.X] : cmd.X) > 0)
+                    CommandPointer += (int)ResolveY(cmd);
+                else
+                    CommandPointer++;
+                break;
+        }
+
+        IsBlocked = false;
+        return true;
+    }
+
+    private long ResolveY(Command cmd) => cmd.YIsRegister ? Reg[cmd.Y] : cmd.Y;
+}
