c8fd5b43f6f32ee66eb5906007c8f5c6a831cf71
Tsehy <cs.mate2000@gmail.com>
2026-01-02 17:25:25 +0100

    2017 Day 23 completed;

---
+++ AdventOfCode/2017/Day23/2017Day23.cs
@@ -0,0 +1,52 @@
+﻿namespace AdventOfCode._2017.Day23;
+
+public class _2017Day23 : _2017Day
+{
+    private readonly List<Command> Commands = [];
+
+    public _2017Day23() : base("Day23")
+    {
+        foreach (string line in Input)
+        {
+            string[] parts = line.Split(' ');
+            Commands.Add(new(parts[0], parts[1], parts[2]));
+        }
+    }
+
+    public override void Part1()
+    {
+        base.Part1();
+
+        var i = new Interpreter(Commands, true);
+        i.Process();
+
+        Console.WriteLine($"The mult command was invoked {i.MulCount} times.");
+    }
+
+    public override void Part2()
+    {
+        base.Part2();
+
+        var i = new Interpreter(Commands, false);
+        i.Process(onlySetup: true);
+
+        int b = (int)i.Reg[1];
+        int c = (int)i.Reg[2];
+        int h = 0;
+        do
+        {
+            for (int d = 2; d < b; d++)
+            {
+                if (b % d == 0)
+                {
+                    h++;
+                    break;
+                }
+            }
+            b += 17;
+        }
+        while (b <= c);
+
+        Console.WriteLine($"The value in register h is: {h}");
+    }
+}
---
+++ AdventOfCode/2017/Day23/Interpreter.cs
@@ -0,0 +1,96 @@
+﻿namespace AdventOfCode._2017.Day23;
+
+internal enum CommandType
+{
+    set, sub, mul, jnz
+}
+
+internal struct Command
+{
+    public CommandType Type;
+    public int X;
+    public int Y;
+    public bool XIsRegister;
+    public bool YIsRegister;
+
+    public Command(string type, string reg1, string reg2)
+    {
+        Type = (CommandType)Enum.Parse(typeof(CommandType), type);
+
+        if (int.TryParse(reg1, out int x))
+        {
+            X = x;
+            XIsRegister = false;
+        }
+        else
+        {
+            X = reg1[0] - 'a';
+            XIsRegister = true;
+        }
+
+        if (int.TryParse(reg2, out int y))
+        {
+            Y = y;
+            YIsRegister = false;
+        }
+        else
+        {
+            Y = reg2[0] - 'a';
+            YIsRegister = true;
+        }
+    }
+
+    public override string ToString() => $"{Type} {X} {Y}";
+}
+
+internal class Interpreter
+{
+    private readonly List<Command> Commands;
+    public int[] Reg { get; } = new int[8];
+
+    public int MulCount { get; private set; } = 0;
+
+    public Interpreter(List<Command> commands, bool debugMode)
+    {
+        Commands = commands;
+        Reg[0] = debugMode ? 0 : 1;
+    }
+
+    public void Process(bool onlySetup = false)
+    {
+        int CommandPointer = 0;
+        int max = onlySetup ? 8 : Commands.Count; // extracted from input
+
+        while (0 <= CommandPointer && CommandPointer < max)
+        {
+            var cmd = Commands[CommandPointer];
+            switch (cmd.Type)
+            {
+                case CommandType.set:
+                    Reg[cmd.X] = ResolveY(cmd);
+                    CommandPointer++;
+                    break;
+
+                case CommandType.sub:
+                    Reg[cmd.X] -= ResolveY(cmd);
+                    CommandPointer++;
+                    break;
+
+                case CommandType.mul:
+                    Reg[cmd.X] *= ResolveY(cmd);
+                    CommandPointer++;
+                    MulCount++;
+                    break;
+
+                case CommandType.jnz:
+                    if ((cmd.XIsRegister ? Reg[cmd.X] : cmd.X) != 0)
+                        CommandPointer += ResolveY(cmd);
+                    else
+                        CommandPointer++;
+                    break;
+            }
+        }
+    }
+
+    private int ResolveY(Command cmd) => cmd.YIsRegister ? Reg[cmd.Y] : cmd.Y;
+}
