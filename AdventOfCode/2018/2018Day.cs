2fd09466d1df4a6f4095fe1df8f757b92116ea54
Tsehy <cs.mate2000@gmail.com>
2026-01-03 12:55:40 +0100

    2018 Day 01 completed;

---
+++ AdventOfCode/2018/2018Day.cs
@@ -0,0 +1,8 @@
+﻿namespace AdventOfCode._2018;
+
+public class _2018Day : Day
+{
+    public _2018Day(string folderName) : base("2018", folderName)
+    {
+    }
+}
---
+++ AdventOfCode/2018/Day01/2018Day01.cs
@@ -0,0 +1,34 @@
+﻿namespace AdventOfCode._2018.Day01;
+
+public class _2018Day01 : _2018Day
+{
+    private readonly int[] Changes;
+
+    public _2018Day01() : base("Day01")
+    {
+        Changes = [.. Input.Select(int.Parse)];
+    }
+
+    public override void Part1()
+    {
+        base.Part1();
+
+        Console.WriteLine(Changes.Sum());
+    }
+
+    public override void Part2()
+    {
+        base.Part2();
+
+        int current = 0, index = 0;
+        var visited = new HashSet<int> { current };
+        do
+        {
+            current += Changes[index];
+            index = (index + 1) % Changes.Length;
+        }
+        while (visited.Add(current));
+
+        Console.WriteLine(current);
+    }
+}
